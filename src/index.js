477499c0f11ce02c043681a0a4d25c5fd0073b95
bhr0554 <bhr5004@gmail.com>
2021-10-27 23:17:02 +0900

    [수정] Hello function

--- src/index.js
+++ src/index.js
@@ -10,13 +10,31 @@ import reportWebVitals from './reportWebVitals';
   </React.StrictMode>,
   document.getElementById('root')
 );*/
-const name = 'HyeRyeon';
+
+/*const name = 'HyeRyeon';
 const element = <h1>Hello, {name}</h1>;
 ReactDOM.render(
     element,
     document.getElementById('root')
+);*/
+
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+    firstName : 'Hye',
+    lastName : 'Park'
+}
+
+const element = (
+    <h1>Hello, {formatName(user)}</h1>
 );
 
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
