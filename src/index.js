c7b87b854ae0253ba33d83f1c25f3c7301122b2d
bhr0554 <bhr5004@gmail.com>
2021-11-05 21:38:22 +0900

    event 처리 및 event 함수 내에서 this binding

--- src/index.js
+++ src/index.js
@@ -4,58 +4,35 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-class Welcome extends React.Component {
+class Toggle extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            name : 'HyePark'
-        }
-        //console.log('constructor 호출');
+        this.state = {isToggleOn: true};
+
+        // 콜백에서 `this`가 작동하려면 아래와 같이 바인딩 해주어야 합니다.
+        this.handleClick = this.handleClick.bind(this);
+        //this.handleClick = this.handleClick.bind({sample:'HyePark'});
+        console.log('constructor this', this);
     }
 
-    static getDerivedStateFromProps(nextProps, prevState) {
-        console.log("getDerivedStateFromProps 호출");
-        debugger;
-        return null;
-    }
-
-    // 확인 못 함. 아마도 setState를 사용하여 확인 가능한 듯.
-    shouldComponentUpdate(nextprops, nextState) {
-        console.log("sholudComponetUpdate 호출")
-        console.log(nextprops, nextState);
-        debugger;
-        return nextprops != nextState;
-    }
-
-    getSnapshotBeforeUpdate(prevProps, prevState) {
-        console.log("getSnapshotBeforeUpdate 호출")
-        debugger;
-        return null;
-    }
-
-    componentDidUpdate() {
-        console.log("componentDidUpdate 호출");
-        debugger;
+    handleClick() {
+        console.log('handleClick this', this);
+        this.setState(prevState => ({
+            isToggleOn: !prevState.isToggleOn
+        }));
     }
 
     render() {
-        console.log("render 호출");
-        debugger;
-        return <div>Hello, {this.state.name}</div>;
+        return (
+            <button onClick={this.handleClick}>
+                {this.state.isToggleOn ? 'ON' : 'OFF'}
+            </button>
+        );
     }
 }
 
-function Welcomes() {
-    return (
-        <div>
-            <Welcome name="q"/>
-            <Welcome name="a"/>
-        </div>
-    );
-}
-
 ReactDOM.render(
-    <Welcomes/>,
+    <Toggle />,
     document.getElementById('root')
 );
 // If you want to start measuring performance in your app, pass a function
